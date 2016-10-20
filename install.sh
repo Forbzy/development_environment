9346589bd645d5d7f71524ecae1686c74918bc5d
root <root@dogbonnah-workstation.dsab-eud-111>
2016-10-20 16:09:21 +0100

    Resized and reallocated space to logical volumes

--- install.sh
+++ install.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
+
+swapoff /dev/vg-workstation/swap
+lvremove /dev/vg-workstation/swap
+lvcreate -L 4G -n swap vg-workstation
+dd if=/dev/zero of=/dev/vg-workstation/swap bs=1M count=4096
+mkswap /dev/vg-workstation/swap
+swapon /dev/vg-workstation/swap
+lvextend -L +125G /dev/vg-workstation/home
+resize2fs /dev/vg-workstation/home
+lvextend -L +5G /dev/vg-workstation/var_log
+resize2fs /dev/vg-workstation/var_log
+lvextend -L +5G /dev/vg-workstation/var_temp
+resize2fs /dev/vg-workstation/var_temp
+lvextend -L +5G /dev/vg-workstation/root
+resize2fs /dev/vg-workstation/root
+lvextend -L +10G /dev/vg-workstation/tmp
+resize2fs /dev/vg-workstation/tmp
+lvextend -L +10G /dev/vg-workstation/var
+resize2fs /dev/vg-workstation/var
+
 set -eux
 
 OS=$(cat /etc/os-release|sed -e 's/"//'|grep ID_LIKE|awk -F '=' '{print $2}'|awk '{print $1}')
@@ -40,15 +60,5 @@ git checkout ${TAG}
 ansible-galaxy install -r requirements.yml
 
 ansible-playbook -i hostfile -v site.yml
-
-swapoff /dev/vg-workstation/swap
-lvremove /dev/vg-workstation/swap
-lvcreate -L 4G -n swap vg-workstation
-dd if=/dev/zero of=/dev/vg-workstation/swap bs=1M count=4096
-mkswap /dev/vg-workstation/swap
-swapon /dev/vg-workstation/swap
-lvextend -L +50G /dev/vg-workstation/home
-lvextend -L +5G /dev/vg-workstation/var_log
-
 
 exit
